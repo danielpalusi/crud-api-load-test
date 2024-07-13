e310bc6f9f8ff1ca24ea79b39f0186e82bd675c0
danielpalusi <35248377+danielpalusi@users.noreply.github.com>
2024-07-13 19:35:59 +0700

    create crud api, add prettierc

--- node-server/server.js
+++ node-server/server.js
@@ -1,11 +1,61 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
+});
+
+let books = [];
+
+app.post('/books', async (req, res) => {
+  const { title, author } = req.body;
+  if (!title || !author) {
+    return res.status(400).send('Missing title or author');
+  }
+
+  const newBook = { id: books.length + 1, title, author };
+  books.push(newBook);
+  res.status(201).send(newBook);
+});
+
+app.get('/books', (req, res) => {
+  res.json(books);
+});
+
+app.get('/books/:id', (req, res) => {
+  const book = books.find(b => b.id === parseInt(req.params.id));
+  if (!book) {
+    return res.status(404).send('Book not found');
+  }
+  res.json(book);
+});
+
+app.put('/books/:id', (req, res) => {
+  const book = books.find(b => b.id === parseInt(req.params.id));
+  if (!book) {
+    return res.status(404).send('Book not found');
+  }
+
+  const { title, author } = req.body;
+  book.title = title || book.title;
+  book.author = author || book.author;
+
+  res.send(book);
+});
+
+app.delete('/books/:id', (req, res) => {
+  const bookIndex = books.findIndex(b => b.id === parseInt(req.params.id));
+  if (bookIndex === -1) {
+    return res.status(404).send('Book not found');
+  }
+
+  books.splice(bookIndex, 1);
+  res.status(204).send();
 });
