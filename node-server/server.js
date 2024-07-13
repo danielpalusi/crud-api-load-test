649231236fdc9cf71b0972f81a0c698ec2c0e193
danielpalusi <35248377+danielpalusi@users.noreply.github.com>
2024-07-13 19:59:34 +0700

    remove unused code

--- node-server/server.js
+++ node-server/server.js
@@ -4,15 +4,15 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+let books = [];
+
+app.get('/', (_, res) => {
   res.send('Hello, World!');
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
-
-let books = [];
 
 app.post('/books', (req, res) => {
   const { title, author } = req.body;
