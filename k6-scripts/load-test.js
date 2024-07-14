775facff15c08717fddd748d2a79c192b6913d28
danielpalusi <35248377+danielpalusi@users.noreply.github.com>
2024-07-14 23:52:57 +0700

    add load-test script for POST book endpoint

--- k6-scripts/load-test.js
+++ k6-scripts/load-test.js
@@ -1,6 +1,8 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+const LOCAL_SERVER = 'http://localhost:3000';
+
 export const options = {
   vus: 10,
   // represents total of virtual users that concurently access the endpoint
@@ -11,7 +13,8 @@ export const options = {
 // k6 will wait for 30 seconds for all virtual users to finish their iterations before stopping
 
 export default function () {
-  let getAllBooks = http.get('http://localhost:3000/books');
+  // GET all books
+  const getAllBooks = http.get(`${LOCAL_SERVER}/books`);
   //fetch the local endpoint
 
   check(getAllBooks, {
@@ -23,5 +26,27 @@ export default function () {
   // checks: 100%,
   // http_req_connecting: avg=14.82µs min=0s    med=0s     max=795µs  p(90)=0s     p(95)=0s
   // vus............................: 10
-  // iteorations : 300
+  // iterations : 300
+
+  // POST Book
+  const bookFormData = {
+    title: 'Book A',
+    author: 'Author A',
+  };
+
+  const postData = http.post(
+    `${LOCAL_SERVER}/books`,
+    JSON.stringify(bookFormData),
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+
+  check(postData, {
+    'Post book with response code 201': res => res.status === 201,
+  });
+
+  // Sun 14/07/24:
+  // checks: 100%,
+  // http_req_connecting: avg=139ns   min=0s       med=0s      max=765µs   p(90)=0s      p(95)=0s
+  // vus............................: 10
+  // iterations : 15364
 }
